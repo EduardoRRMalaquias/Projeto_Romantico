c28e8e794e92cae7cc34afafcccf1f5ada3255a8
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2025-02-21 08:49:28 -0300

    coracaoes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,50 @@
+'use client';
 import Carousel from '@/components/Carousel';
 import RomanticBanner from '@/components/RomanticBanner';
 import RomanticFooter from '@/components/RomanticFooter';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    // Função para criar a partícula de coração
+    const createHeartParticle = () => {
+      const particle = document.createElement('div');
+      particle.classList.add('particle');
+
+      // Define posições iniciais aleatórias na tela
+      const startX = Math.random() * window.innerWidth;
+      const startY = Math.random() * window.innerHeight;
+
+      // Define o tempo de vida da partícula (quanto tempo ela vai aparecer)
+      const lifetime = 1000; // Tempo de vida de cada partícula (1 segundo)
+
+      // Adiciona partículas à tela
+      particle.style.left = `${startX}px`;
+      particle.style.top = `${startY}px`;
+
+      // Adiciona a partícula ao corpo
+      document.body.appendChild(particle);
+
+      // Remove a partícula após o tempo de vida (1 segundo)
+      setTimeout(() => {
+        particle.remove();
+      }, lifetime);
+    };
+
+    // Cria 100 partículas logo ao carregar
+    for (let i = 0; i < 100; i++) {
+      createHeartParticle();
+    }
+  }, []);
+
   return (
     <main
-      style={{ paddingLeft: '32px', paddingRight: "32px" }}
-      className="flex flex-col gap-8 row-start-2 items-center sm:items-start"
+      style={{
+        paddingLeft: '32px',
+        paddingRight: '32px',
+      }}
+      className="App flex flex-col gap-8 row-start-2 items-center sm:items-start"
     >
       <RomanticBanner
         title="Para a princesa mais especial❤️"
